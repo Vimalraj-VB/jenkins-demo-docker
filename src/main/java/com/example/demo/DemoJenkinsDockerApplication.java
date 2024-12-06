68e4e55311e5468059e8f646d73d7b858a8090b8
vimalraj-tecdata <vimalraj@tecdata.co.in>
2024-12-06 11:11:42 +0530

    add sysyout

--- src/main/java/com/example/demo/DemoJenkinsDockerApplication.java
+++ src/main/java/com/example/demo/DemoJenkinsDockerApplication.java
@@ -8,6 +8,7 @@ public class DemoJenkinsDockerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJenkinsDockerApplication.class, args);
+		System.out.println("The Application is Started ........ !!!!");
 	}
 
 }
